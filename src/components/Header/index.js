7f7a6397d9e2659aa4591b87629a7b163f318904
Felipe Genuino <design@felipegenuino.com>
2022-09-25 10:32:41 -0300

    create header

---
+++ src/components/Header/index.js
@@ -0,0 +1,9 @@
+ 
+export default function Header(){
+    return(
+        <header>
+            <h1>Breweries </h1>
+            <h2> A breweries list by Open Brewery DB </h2>
+        </header>
+    )
+}
